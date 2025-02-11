79500d2bdf9d69fc6174596f3c33dca3ae5bdc28
허강현 <hgh6128@gmail.com>
2025-02-11 13:21:53 +0900

    팔로우, 해시태그 검색 기능

--- controllers/page.js
+++ controllers/page.js
@@ -1,5 +1,6 @@
 const Post = require('../models/post');
 const User = require('../models/user');
+const Hashtag = require('../models/hashtag');
 
 exports.renderProfile = (req, res, next) => {
     // 서비스를 호출
@@ -29,4 +30,28 @@ exports.renderMain = async (req, res, next) => {
     }
 };
 
+exports.renderHashtag = async (req, res, next) => {
+    console.log(req.query);
+    const query = req.query.hashtag;
+    if (!query) {
+        return res.redirect('/');
+    }
+    try {
+        const hashtag = await Hashtag.findOne({ where: { title: query } });
+        let posts = [];
+        if (hashtag) {
+            posts = await hashtag.getPosts({
+                include: [{ model: User, attributes: ['id', 'nick'] }],
+                order: [['createdAt', 'DESC']],
+            });
+        }
+        res.render('main', {
+            title: `${query} | NodeBird`,
+            twits: posts,
+        });
+    } catch (error) {
+        console.error(error);
+        next(error);
+    }
+};
 // 라우터 -> 컨트롤러 -> 서비스(요청, 응답 모름)
